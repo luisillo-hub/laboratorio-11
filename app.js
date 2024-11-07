333dcf97f31f6ac16f36039212bfd457923e07e4
Luis Fernando de Jesus Diaz Rojas <luisfernandogames2.0@gmail.com>
2024-11-07 18:50:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ function VerificarCalificacion() {
     let calificacion = parseInt(document.getElementById("calificacion").value);
     let mensaje = "";
     if (calificacion >= 90) {
-       mensaje = "Aprobado con Honores."); 
+       mensaje = "Aprobado con Honores."; 
     }else if (calificacion>=70) {
-        document.write("Aprobado."); 
+         mensaje = "Aprobado.";
     }else{
-        document.write("Reprobado."); 
+        mensaje = "Reprobado."; 
     }
     document.getElementById("gradosalida").innerText = mensaje;
 }
 function determinarParidad() {
-let numero = prompt("Escribe un numero del 1 al 100: ");
-if numero%2 == 0 {
-    document.write("El numero es par. <br>"); 
-}else {
-    document.write("El numero es impar. <br>"); 
-}
+    let numero = parseInt(document.getElementById("numero").value);
+    let mensaje = "";
+    if (numero%2 == 0) {
+        mensaje = "El numero es par."; 
+    }else {
+        mensaje = "El numero es impar."; 
+    }
+    document.getElementById("gradosalida").innerText = mensaje;
 }
 function evaluarDescuento() {
-let monto = prompt("Escribe un numero del 1 al 100: ");
+    let monto = parseInt(document.getElementById("monto").value);
+    let mensaje = "";    
+    if (monto > 100) {
     
+    }
 }
 function jugarAdivinanza() {
-
+    let random = rand(0-10); 
+    let adivinanza = parseInt(document.getElementById("adivinanza").value);
+    let mensaje = "";
 }
