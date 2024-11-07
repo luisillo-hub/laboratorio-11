b184efb163d2d517eed936c2f11bdd1c6b8813c1
Luis Fernando de Jesus Diaz Rojas <luisfernandogames2.0@gmail.com>
2024-11-07 16:04:58 -0500

    lo_intente

---
+++ app.js
@@ -0,0 +1,22 @@
+function mostrarValores() {
+    let calificacion = 70;
+    if calificacion >= 90 {
+       document.write("Aprobado con Honores. <br>"); 
+    }else if calificacion>=70 || calificacion <90 {
+        document.write("Aprobado. <br>"); 
+    }else{
+        document.write("Reprobado. <br>"); 
+    }
+} 
+function VerificarCalificacion() {
+
+}
+function determinarParidad() {
+
+}
+function evaluarDescuento() {
+
+}
+function jugarAdivinanza() {
+
+}
